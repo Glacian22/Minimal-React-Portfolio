0854beb567ef8b4488fddd929be1e3c245d79449
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2020-01-20 12:24:52 -0800

    added more slides, fixing css

--- src/components/views/portfolio/portfolio.js
+++ src/components/views/portfolio/portfolio.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { Carousel } from 'react-responsive-carousel'
 import inTheirShoesFront from '../../../images/portfolio/lobby.jpg'
 import inTheirShoesBack from '../../../images/portfolio/socketlog.png'
+import thisPortfolio from '../../../images/portfolio/Portfolio_small.jpg'
 import './style.css'
 
 export default function Portfolio (props) {
@@ -14,7 +16,7 @@ export default function Portfolio (props) {
           <div>
             <img src={inTheirShoesFront} alt='In Their Shoes React' />
             <p class='card-text'>In Their Shoes client</p>
-            <p>Group training tool for non-profits.
+            <p id='boom'>Group training tool for non-profits.
                Uses React for UI, with sockets for realtime communication with the server.</p>
           </div>
         </a>
@@ -27,6 +29,15 @@ export default function Portfolio (props) {
             </p>
           </div>
         </a>
+        <Link className='folioCard' to='about'>
+          <div>
+            <img src={thisPortfolio} alt='this site' />
+            <p class='card-text'>This Portfolio</p>
+            <p> This portfolio site was designed and built by me, using React with custom components
+              and leveraging CSS3 grid.
+            </p>
+          </div>
+        </Link>
       </Carousel>
     </div>
   )
