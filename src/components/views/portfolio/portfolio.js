9ff51864810717755b5c11c2d6e1684c120e1cb2
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2020-01-20 00:09:56 -0800

    smoothing out portfolio

--- src/components/views/portfolio/portfolio.js
+++ src/components/views/portfolio/portfolio.js
@@ -8,17 +8,23 @@ import './style.css'
 export default function Portfolio (props) {
   return (
     <div className='content' id='portfolio'>
+      <p>Some projects I have worked on:</p>
       <Carousel showThumbs={false} showStatus={false} infiniteLoop>
         <a className='folioCard' href='https://github.com/aprilleperez/realgameoflife_client' target='blank'>
           <div>
             <img src={inTheirShoesFront} alt='In Their Shoes React' />
-            <p class='card-text'>In Their Shoes client - full stack game app/platform frontend</p>
+            <p class='card-text'>In Their Shoes client</p>
+            <p>Group training tool for non-profits.
+               Uses React for UI, with sockets for realtime communication with the server.</p>
           </div>
         </a>
         <a className='folioCard' href='https://github.com/cplank/LifeAPI' target='blank'>
           <div>
             <img src={inTheirShoesBack} alt='In Their Shoes Server' />
-            <p class='card-text'>In Their Shoes Server - sockets4days</p>
+            <p class='card-text'>In Their Shoes Server</p>
+            <p>The backend for this app was built with Node, Express, sockets, and uses MongoDB to store
+              scenario templates and user info.
+            </p>
           </div>
         </a>
       </Carousel>
