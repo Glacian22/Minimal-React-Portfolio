369d9fbbae24d46a1268fbf61e0e042520c8ad98
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2020-01-20 15:13:48 -0800

    about page oops

--- src/components/views/about/about.js
+++ src/components/views/about/about.js
@@ -5,9 +5,16 @@ export default function About (props) {
   return (
     <div className='content' id='about'>
       <div>
-        <p>Species: Human</p>
-        <p>Occupation: Software Developer</p>
-        <p>Disposition: Perpetually Curious/Hungry</p>
+        <p><bdi className='about-headers'>Species:</bdi> Human</p>
+        <p><bdi className='about-headers'>Occupation:</bdi> Software Developer</p>
+        <p><bdi className='about-headers'>Disposition:</bdi> Curious/Hungry</p>
+        <p><bdi className='about-headers'>Background: </bdi>
+           I've been writing code in one form or another since I was 12. These days I
+          specialize in full-stack development, creating websites, desktop and mobile applications,
+          and assorted other projects. I enjoy tackling tough problems, whether it's with an existing
+          code base or starting from scratch, familiar tech or something brand new. Just keep me full of
+          snacks and coffee and we're good to go.
+        </p>
       </div>
     </div>
   )
--- src/components/views/portfolio/portfolio.js
+++ src/components/views/portfolio/portfolio.js
@@ -5,6 +5,9 @@ import { Carousel } from 'react-responsive-carousel'
 import inTheirShoesFront from '../../../images/portfolio/lobby.jpg'
 import inTheirShoesBack from '../../../images/portfolio/socketlog.png'
 import thisPortfolio from '../../../images/portfolio/Portfolio_small.jpg'
+import blobber from '../../../images/portfolio/blobber.jpg'
+import squares from '../../../images/portfolio/squares.gif'
+import watDis from '../../../images/portfolio/WatDis.png'
 import './style.css'
 
 export default function Portfolio (props) {
@@ -15,7 +18,7 @@ export default function Portfolio (props) {
         <a className='folioCard' href='https://github.com/aprilleperez/realgameoflife_client' target='blank'>
           <div>
             <img src={inTheirShoesFront} alt='In Their Shoes React' />
-            <p class='card-text'>In Their Shoes client</p>
+            <div class='card-title'>In Their Shoes client</div>
             <p id='boom'>Group training tool for non-profits.
                Uses React for UI, with sockets for realtime communication with the server.</p>
           </div>
@@ -23,7 +26,7 @@ export default function Portfolio (props) {
         <a className='folioCard' href='https://github.com/cplank/LifeAPI' target='blank'>
           <div>
             <img src={inTheirShoesBack} alt='In Their Shoes Server' />
-            <p class='card-text'>In Their Shoes Server</p>
+            <div class='card-title'>In Their Shoes Server</div>
             <p>The backend for this app was built with Node, Express, sockets, and uses MongoDB to store
               scenario templates and user info.
             </p>
@@ -32,12 +35,43 @@ export default function Portfolio (props) {
         <Link className='folioCard' to='about'>
           <div>
             <img src={thisPortfolio} alt='this site' />
-            <p class='card-text'>This Portfolio</p>
+            <div class='card-title'>This Portfolio</div>
             <p> This portfolio site was designed and built by me, using React with custom components
               and leveraging CSS3 grid.
             </p>
           </div>
         </Link>
+        <a className='folioCard' href='https://github.com/yomyyo/yelling-game' target='blank'>
+          <div>
+            <img src={blobber} alt='Blobber Royale' />
+            <div class='card-title'>Blobber Royale</div>
+            <p>Blobber Royale - full stack networked game built on sockets and MS Azure realtime speech recognition API
+              <br />
+               The code for this game is rough around the edges, and is being completely reworked for mobile deployment.
+            </p>
+          </div>
+        </a>
+        <a className='folioCard' href='https://github.com/Glacian22/React-Tutorial' target='blank'>
+          <div>
+            <img src={squares} alt='React Tutorial' />
+            <div class='card-title'>React Tutorial</div>
+            <p>A demo of React concepts I built as a tutorial for my coding bootcamp students.
+              <br />
+              Designed to show how to structure a React app, use props, state, hooks, and the usefulness of nesting components as children.
+            </p>
+          </div>
+        </a>
+        <a className='folioCard' href='https://github.com/QuantumPhilosophy/anonymous-pumpkin' target='blank'>
+          <div>
+            <img src={watDis} alt='Wat Dis' />
+            <div class='card-title'>Wat Dis</div>
+            <p> Wat Dis?
+              <br />
+              Wat Dis? - frontend web app for identifying and elaborating on the contents of
+                images. Uses Google Cloud Vision and Wikipedia APIs</p>
+          </div>
+        </a>
+
       </Carousel>
     </div>
   )
