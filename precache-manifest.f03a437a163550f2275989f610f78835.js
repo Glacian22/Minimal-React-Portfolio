dd1e7ac7d43608008ad50431374b11c0110ebd56
Abram Thau <glacian22@gmail.com>
2020-01-21 12:09:58 -0800

    Updates

---
+++ precache-manifest.f03a437a163550f2275989f610f78835.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f781ea2da55d267bcaacef6d1acd1e9",
+    "url": "/Minimal-React-Portfolio/index.html"
+  },
+  {
+    "revision": "b402d852997a51a86809",
+    "url": "/Minimal-React-Portfolio/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "38aeee35ae4ad401e8b3",
+    "url": "/Minimal-React-Portfolio/static/css/main.52981d0d.chunk.css"
+  },
+  {
+    "revision": "b402d852997a51a86809",
+    "url": "/Minimal-React-Portfolio/static/js/2.e45687f4.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/Minimal-React-Portfolio/static/js/2.e45687f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38aeee35ae4ad401e8b3",
+    "url": "/Minimal-React-Portfolio/static/js/main.8d4d1451.chunk.js"
+  },
+  {
+    "revision": "0cc202b6506f1e1f296b",
+    "url": "/Minimal-React-Portfolio/static/js/runtime-main.00f3691c.js"
+  },
+  {
+    "revision": "172901b2a747ec84c147eee508194435",
+    "url": "/Minimal-React-Portfolio/static/media/WatDis.172901b2.png"
+  },
+  {
+    "revision": "6c5cf54b6f4d88bc8920c1bc2cbc0cac",
+    "url": "/Minimal-React-Portfolio/static/media/blobber.6c5cf54b.jpg"
+  },
+  {
+    "revision": "e6e1f1672e0be56b0c78de9799074087",
+    "url": "/Minimal-React-Portfolio/static/media/github.e6e1f167.png"
+  },
+  {
+    "revision": "c6965b9aea53a1965f5230ff32ae7f26",
+    "url": "/Minimal-React-Portfolio/static/media/lobby.c6965b9a.jpg"
+  },
+  {
+    "revision": "c7d983ade3d33faf5b480e71443ad74d",
+    "url": "/Minimal-React-Portfolio/static/media/socketlog.c7d983ad.png"
+  },
+  {
+    "revision": "976fcf560eec760a8b7afca7c8fd5b69",
+    "url": "/Minimal-React-Portfolio/static/media/squares.976fcf56.gif"
+  }
+]);
