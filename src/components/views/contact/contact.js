faf6740df22fda1bc570c501ffc84eef73f7e21f
Abram Thau <glacian22@gmail.com>
2020-01-21 09:28:17 -0800

    email form working?

--- src/components/views/contact/contact.js
+++ src/components/views/contact/contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import mailer from 'nodemailer'
+import emailjs from 'emailjs-com'
 import './style.css'
 
 export default function Contact (props) {
@@ -9,7 +9,15 @@ export default function Contact (props) {
 
   let handleSubmit = event => {
     event.preventDefault()
+    let mailKey = process.env.EMAIL_KEY
     console.log(event.target['name'].value)
+    console.log(mailKey)
+    emailjs.sendForm('portfolio', 'contact_form', event.target, mailKey)
+      .then((result) => {
+        console.log(result.text)
+      }, (error) => {
+        console.log(`error: ${error.text}`)
+      })
   }
 
   return (
@@ -17,10 +25,10 @@ export default function Contact (props) {
       <form id='contact-form' onSubmit={handleSubmit}>
         <label>
           Contact
-          <input type='text' id='name' value={name} onChange={(event) => setName(event.target.value)} />
-          <input type='text' id='email' value={email} onChange={(event) => setEmail(event.target.value)} />
+          <input type='text' name='name' id='name' value={name} onChange={(event) => setName(event.target.value)} />
+          <input type='text' name='email' id='email' value={email} onChange={(event) => setEmail(event.target.value)} />
           <br />
-          <textarea id='message' value={message} onChange={(event) => setMessage(event.target.value)} />
+          <textarea name='text' id='message' value={message} onChange={(event) => setMessage(event.target.value)} />
           <br />
         </label>
         <input type='submit' value='submit' id='submitbtn' />
