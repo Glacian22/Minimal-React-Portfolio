2d557b8fa6627b123d1065755e4c75dab2ab9ccc
Abram Thau <glacian22@gmail.com>
2020-01-21 13:43:49 -0800

    contact looking decent

--- src/components/views/contact/contact.js
+++ src/components/views/contact/contact.js
@@ -13,23 +13,24 @@ export default function Contact (props) {
     emailjs.sendForm('portfolio', 'contact_form', event.target, mailKey)
       .then((result) => {
         console.log(result.text)
+        setName('')
+        setEmail('')
+        setMessage('')
+        window.alert('Your message is on its way, thanks!')
       }, (error) => {
         console.log(`error: ${error.text}`)
       })
   }
 
   return (
-    <div id='contact'>
-      contact area
+    <div className='content' id='contact'>
       <form id='contact-form' onSubmit={handleSubmit}>
-        <label>
-          Contact
-          <input type='text' name='name' id='name' value={name} onChange={(event) => setName(event.target.value)} />
-          <input type='text' name='email' id='email' value={email} onChange={(event) => setEmail(event.target.value)} />
-          <br />
-          <textarea name='text' id='message' value={message} onChange={(event) => setMessage(event.target.value)} />
-          <br />
-        </label>
+        <label for='name'>{'Name: '}</label>
+        <input type='text' name='name' id='name' value={name} onChange={(event) => setName(event.target.value)} />
+        <label for='email'>{'Email: '}</label>
+        <input type='text' name='email' id='email' value={email} onChange={(event) => setEmail(event.target.value)} />
+        <label for='message'>{'Message: '}</label>
+        <textarea name='text' id='message' value={message} onChange={(event) => setMessage(event.target.value)} />
         <input type='submit' value='submit' id='submitbtn' />
       </form>
     </div>
