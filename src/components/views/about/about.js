6e9ffbdb8658f0b17f54f25a7995587ac0a21317
Abram Thau <glacian22@gmail.com>
2023-02-07 23:03:33 -0800

    working, but inf loop

--- src/components/views/about/about.js
+++ src/components/views/about/about.js
@@ -1,59 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
-export default class About extends React.Component {
-  state = {
-    name: '',
-    species: '',
-    occ: '',
-    disp: '',
-    background: ''
-  }
-  text = {
-    name: 'Abram Thau',
-    species: 'Human',
-    occ: 'Software Developer',
-    disp: 'Curious/Snacky',
-    background: `I've been a writer, inventor, and entrepeneur, but these days I
+let About = () => {
+
+  const [dispText, setDispText] = useState({name: '', species: '', occ: '', disp: '', background: ''})
+
+  const text = [
+    ['name', 'Abram Thau'],
+    ['species', 'Human'],
+    ['occ', 'Software Developer'],
+    ['disp', 'Curious/Snacky'],
+    ['background', `I've been a writer, inventor, and entrepeneur, but these days I
     specialize in full-stack development. You'll find me creating websites, desktop and mobile applications,
     and assorted other projects. I enjoy tackling tough problems, whether it's
     with an existing code base or starting from scratch, familiar tech or something brand new.
-    Just keep me full of snacks and point me at a goal.`
-  }
+    Just keep me full of snacks and point me at a goal.`]
+  ]
 
-  componentDidMount (){
-    let count = 1
-    for (const key in this.text) {
-      setTimeout(() => this.showText(key), count * 1300)
-      count++
-    }
-  }
+  // new algo: 
+  //  update 1 letter per function call, 
+  //  set time out to call funtion again if total not met
 
-  showText = (key) => {
-    let count = 0
-    const length = this.text[key].length
-    let speed = length < 30 ? 50 : 5
-    let timer = setInterval(() => {
-      this.setState({[key]: this.text[key].substr(0, count)})
-      count++
-      if (count > this.text[key].length) {
-        clearInterval(timer)
+  useEffect(() => {
+    const showText = (row = 0) => { 
+      const key = text[row][0];
+      let newRow;
+
+      setDispText(dTxt => {
+        // if done with this row, don't update state and check if there are more rows. If so, increment row if more rows available, or set to -1 to indicate all done.
+        // else update state with additional char, run again
+        console.log(row, key)
+        console.log(dTxt[key].length, text[row][1].length)
+        if (dTxt[key].length === text[row][1].length) {
+          console.log('next row')
+          newRow = row < text.length - 1 ? row + 1 : -1;
+          return dTxt;
+        }
+        else {
+          console.log('printing')
+          return {...dTxt, [key]: text[row][1].substr(0, dTxt[key].length + 1)}
+        }
+      })
+
+      // Logic to decide if to call showText again, ie more text to render
+      // setTimeOut showText()
+      if (newRow !== -1){
+        setTimeout(() => {
+          showText(newRow)
+        }, 2000)
       }
-    }, speed)
-  }
+    }
+      
+    showText()
+  }, [])
 
-  render() {
-    return (
-      <div className='content' id='about'>
-        <div>
-          <p><bdi className='about-headers'>Name:</bdi> {this.state.name}</p>
-          <p><bdi className='about-headers'>Species:</bdi> {this.state.species}</p>
-          <p><bdi className='about-headers'>Occupation:</bdi> {this.state.occ}</p>
-          <p><bdi className='about-headers'>Disposition:</bdi> {this.state.disp}</p>
-          <p><bdi className='about-headers'>Background: </bdi> {this.state.background}
-          </p>
-        </div>
+
+  return (
+    <div className='content' id='about'>
+      <div>
+        <p className='about-headers'>Name: {dispText.name}</p>
+        <p className='about-headers'>Species: {dispText.species}</p>
+        <p className='about-headers'>Occupation:{dispText.occ}</p>
+        <p className='about-headers'>Disposition: {dispText.disp}</p>
+        <p className='about-headers'>Background: {dispText.background}</p>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default About;
---
+++ src/components/views/about/about_old.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './style.css'
+
+export default class About extends React.Component {
+  state = {
+    name: '',
+    species: '',
+    occ: '',
+    disp: '',
+    background: ''
+  }
+  text = {
+    name: 'Abram Thau',
+    species: 'Human',
+    occ: 'Software Developer',
+    disp: 'Curious/Snacky',
+    background: `I've been a writer, inventor, and entrepeneur, but these days I
+    specialize in full-stack development. You'll find me creating websites, desktop and mobile applications,
+    and assorted other projects. I enjoy tackling tough problems, whether it's
+    with an existing code base or starting from scratch, familiar tech or something brand new.
+    Just keep me full of snacks and point me at a goal.`
+  }
+
+  componentDidMount (){
+    let count = 1
+    for (const key in this.text) {
+      setTimeout(() => this.showText(key), count * 1300)
+      count++
+    }
+  }
+
+  showText = (key) => {
+    let count = 0
+    const length = this.text[key].length
+    let speed = length < 30 ? 50 : 5
+    let timer = setInterval(() => {
+      this.setState({[key]: this.text[key].substr(0, count)})
+      count++
+      if (count > this.text[key].length) {
+        clearInterval(timer)
+      }
+    }, speed)
+  }
+
+  render() {
+    return (
+      <div className='content' id='about'>
+        <div>
+          <p><bdi className='about-headers'>Name:</bdi> {this.state.name}</p>
+          <p><bdi className='about-headers'>Species:</bdi> {this.state.species}</p>
+          <p><bdi className='about-headers'>Occupation:</bdi> {this.state.occ}</p>
+          <p><bdi className='about-headers'>Disposition:</bdi> {this.state.disp}</p>
+          <p><bdi className='about-headers'>Background: </bdi> {this.state.background}
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
